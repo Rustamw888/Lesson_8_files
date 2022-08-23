5de1f0791d43388630651620de2df4739c7570b5
qaMilord <rustamw888@gmail.com>
2022-08-23 22:21:40 +0300

    homework done

--- tests/test_homework_files.py
+++ tests/test_homework_files.py
@@ -19,31 +19,31 @@ def test_zip_files():
 
 def test_unpacked_files():
     fantasy_zip = zipfile.ZipFile('../resources/archive_files.zip')
-    fantasy_zip.extractall('../unpacked_files/')
+    fantasy_zip.extractall('../tests/unpacked_files/')
     fantasy_zip.close()
 
 
 def test_csv():
-    with open('unpacked_files/csvSample.csv') as f:
+    with open('../tests/unpacked_files/csvSample.csv') as f:
         reader = csv.reader(f)
         headers = next(reader)
     assert 'Data_value' in str(headers)
 
 
 def test_pdf():
-    reader = PdfReader('unpacked_files/pdfSample.pdf')
+    reader = PdfReader('../tests/unpacked_files/pdfSample.pdf')
     page = reader.pages[2]
     text = page.extract_text()
     assert 'Background' in text
 
 
 def test_txt():
-    reader = open('unpacked_files/txtSample.txt')
+    reader = open('../tests/unpacked_files/txtSample.txt')
     assert reader.readline() == 'Quod equidem non reprehendo;\n'
 
 
 def test_xlsx():
-    workbook = load_workbook('unpacked_files/xlsxSample.xlsx')
+    workbook = load_workbook('../tests/unpacked_files/xlsxSample.xlsx')
     sheet = workbook.active
     result = sheet.cell(row=32, column=6).value
     assert 1875 == result
